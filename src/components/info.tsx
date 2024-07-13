0c789d2afec286cbfa8e24c2f24b3b03d095bfae
Cxyu629 <chowxingyu123@gmail.com>
2024-07-14 02:29:42 +0800

    Fixed blank unicode character

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -3,7 +3,7 @@ import { VoidComponent } from "solid-js";
 const Info: VoidComponent<{ tooltip: string }> = (props) => {
   return (
     <p data-before={props.tooltip} class="relative before:absolute before:bottom-[430%] before:left-1/2 before:text-center before:w-40 before:-m-20 before:bg-[#242424] before:z-10 before:border before:rounded-lg before:py-1 before:content-[attr(data-before)] hover:before:visible before:invisible">
-      🛈
+      ⓘ
     </p>
   );
 };
